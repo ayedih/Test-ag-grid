09de996cd0a9020d3f2bfda31d30b804f08da572
ayedih <ayedih123@gmail.com>
2019-12-12 11:00:19 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+
+// HttpClient
+import { HttpClientModule } from "@angular/common/http";
+
+// ag-grid
+import { AgGridModule } from "@ag-grid-community/angular";
+
+//AgGridModule.withComponents([]) => the withComponent call is necessary for the grid to be able to use Angular Components as cells/headers
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule,  AgGridModule.withComponents([]) ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
