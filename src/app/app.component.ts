5d0285c41dd847e029f8fe5590928e0049fb0e68
ayedih <ayedih123@gmail.com>
2019-12-12 13:52:32 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, ViewChild } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {AllModules} from "@ag-grid-enterprise/all-modules";
+import { Component, ViewChild } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AllModules } from "@ag-grid-enterprise/all-modules";
 import "@ag-grid-community/all-modules/dist/styles/ag-grid.css";
 import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-
+export class AppComponent {
   private gridApi;
   private gridColumnApi;
 
@@ -18,15 +17,14 @@ export class AppComponent  {
 
   private columnDefs;
   private defaultColDef;
-  private rowData : any[];
+  private rowData: any[];
 
-  constructor(private http:HttpClient){
-
-     this.columnDefs = [
+  constructor(private http: HttpClient) {
+    this.columnDefs = [
       {
         headerName: "Athlete",
         field: "athlete",
-        width: 150,
+        width: 150
       },
       {
         headerName: "Age",
@@ -48,7 +46,7 @@ export class AppComponent  {
         headerName: "Date",
         field: "date",
         width: 110,
-       /*  comparator: dateComparator */
+        comparator: dateComparator
       },
       {
         headerName: "Medals",
@@ -81,41 +79,36 @@ export class AppComponent  {
       }
     ];
 
-      this.defaultColDef = {
+    this.defaultColDef = {
       sortable: true,
       resizable: true
     };
   }
 
-
   printState() {
+    var colState = this.gridColumnApi.getColumnState(); //Gets the state of the columns. Typically used when saving column state.
+    var groupState = this.gridColumnApi.getColumnGroupState(); //Gets the state of the column groups. Typically used when saving column group state.
+    var sortState = this.gridApi.getSortModel();
+    var filterState = this.gridApi.getFilterModel();
 
-        var colState = this.gridColumnApi.getColumnState();//Gets the state of the columns. Typically used when saving column state.
-        var groupState = this.gridColumnApi.getColumnGroupState();//Gets the state of the column groups. Typically used when saving column group state.
-        var sortState = this.gridApi.getSortModel();
-        var filterState = this.gridApi.getFilterModel();
-
-        console.log("******************************");
-        console.log("colState: ", colState);
-        console.log("groupState: ", groupState);
-        console.log("sortState: ", sortState);
-        console.log("filterState: ", filterState);
-        console.log("******************************");
+    console.log("******************************");
+    console.log("colState: ", colState);
+    console.log("groupState: ", groupState);
+    console.log("sortState: ", sortState);
+    console.log("filterState: ", filterState);
+    console.log("******************************");
   }
 
-
   saveState() {
-
     window.colState = this.gridColumnApi.getColumnState();
     window.grouState = this.gridColumnApi.getColumnGroupState();
     window.sortState = this.gridColumnApi.getSortModel();
     window.filterState = this.gridColumnApi.getFilterModel();
-    
+
     console.log("column state saved");
   }
 
   restoreState() {
-
     if (!window.colState) {
       console.log("no columns state to restore by, you must save state first");
       return;
@@ -129,21 +122,22 @@ export class AppComponent  {
     console.log("column state restored");
   }
 
-
   resetState() {
     this.gridColumnApi.resetColumnState();
     this.gridColumnApi.resetColumnGroupstate();
     this.gridApi.setSortModel(null);
     this.gridApi.setFilterModel(null);
-
   }
 
   showAthlete(show) {
     this.gridColumnApi.setColumnVisible("athlete", show);
   }
 
-   showMedals(show) {
-    this.gridColumnApi.setColumnsVisible(["total", "gold", "silver", "bronze"], show);
+  showMedals(show) {
+    this.gridColumnApi.setColumnsVisible(
+      ["total", "gold", "silver", "bronze"],
+      show
+    );
   }
 
   pinAthlete(pin) {
@@ -151,17 +145,58 @@ export class AppComponent  {
   }
 
   pinAge(pin) {
-        this.gridColumnApi.setColumnPinned("age", pin);
-
+    this.gridColumnApi.setColumnPinned("age", pin);
   }
 
   onGridReady(params) {
-
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
 
-    this.http.get(  "https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json").subscribe(date => {
-      this.rowData = data;
+    this.http
+      .get(
+        "https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json"
+      )
+      .subscribe(date => {
+        this.rowData = data;
+      });
+
+    params.api.addGlobalListener(function(type, event) {
+      if (type.indexOf("column") >= 0) {
+        console.log("Got column event: ", event);
+      }
     });
   }
- }
+}
+
+function dateComparator(date1, date2) {
+  var date1Number = monthToComparableNumber(date1);
+  var date2Number = monthToComparableNumber(date2);
+
+  if (date1Number === null && date2Number === null) {
+    return 0;
+  }
+
+  if (date1Number === null) {
+    return -1;
+  }
+
+  if (date2Number === null) {
+    return 1;
+  }
+
+  return date1Number - date2Number;
+}
+
+function monthToComparableNumber(date) {
+  if (date === undefined || date === null || date.length !== 10) {
+    return null;
+  }
+
+  var yearNumber = date.subString(6, 10);
+  var monthNumber = date.subString(3, 5);
+  var dayNumber = date.subString(0, 2);
+
+  var result = yearNumber * 10000 + monthNumber * 100 + dayNumber;
+
+  return result;
+}
